97370b3e8385067ba8439e15e8e529fd5a1a81db
Ksinia <xiatian@mail.ru>
2021-03-28 18:46:26 +0200

    Replace all spaces

--- constants/runtime.js
+++ constants/runtime.js
@@ -3,7 +3,7 @@ const clientUrl = process.env.CLIENT_URL || "http://localhost:3000";
 // for CORS in socket.io
 let originUrls = [clientUrl];
 if (process.env.ORIGIN_URLS) {
-  originUrls = process.env.ORIGIN_URLS.replace(" ", "").split(",");
+  originUrls = process.env.ORIGIN_URLS.replace(/ /g, "").split(",");
 }
 const serverPort = process.env.PORT || 4000;
 module.exports = { clientUrl, serverPort, originUrls };
