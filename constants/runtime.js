afe66807bb9f3c3f186bbd3f00313a6f4341cebc
Ksinia <xiatian@mail.ru>
2021-03-28 18:42:21 +0200

    Fix runtime.js

--- constants/runtime.js
+++ constants/runtime.js
@@ -5,7 +5,5 @@ let originUrls = [clientUrl];
 if (process.env.ORIGIN_URLS) {
   originUrls = process.env.ORIGIN_URLS.replace(" ", "").split(",");
 }
-originUrls = "test, test2 ".replace(/ /g, "").split(",");
-console.log(originUrls);
 const serverPort = process.env.PORT || 4000;
 module.exports = { clientUrl, serverPort, originUrls };
