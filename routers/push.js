1f63c3f56be5715d361a5d9ef05254d071aa475b
arusanov <alexander.rusanov@gmail.com>
2022-05-04 18:06:51 +0200

    not ok

--- routers/push.js
+++ routers/push.js
@@ -5,13 +5,16 @@ const authMiddleware = require("../auth/middleware");
 const router = new Router();
 
 router.post("/subscribe", authMiddleware, async (req, res) => {
-  if (!req.body.password) {
+  if (req.body) {
     //STORE IN THE DB!!!
     addSubscription(req.user.id, req.body);
     res.status(200).send({
       message: "OK",
     });
-    return;
+  } else {
+    res.status(400).send({
+      message: "NOT OK",
+    });
   }
 });
 
